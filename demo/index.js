8653eccb7e889e5cbea6cfc55817703688f6f38e
zhouhuafei <zhouhuafei@shopex.cn>
2018-03-19 16:28:32 +0800

    test

--- demo/index.js
+++ demo/index.js
@@ -1,27 +1,35 @@
+// 豆瓣的jsonp接口 https://api.douban.com/v2/book/search?q=javascript&count=1&callback=handleResponse
 const jsonp = require('../dist/index.min');
 const wrap = document.querySelector('.wrap');
+// 测试成功状态
 jsonp({
-    url: 'http://www.suibianxiexie.top/phone/api/verify-code-register-random/',
+    url: `https://api.douban.com/v2/book/search`,
     data: {
-        isJsonp: true,
-        username: '1123486116@qq.com',
+        q: 'javascript',
+        count: 100,
     },
     callback: function (error, dataInfo) {
         const div = document.createElement('div');
-        div.innerHTML = `<div>error：${JSON.stringify(error)}</div><div>dataInfo：${JSON.stringify(dataInfo)}</div>`;
+        div.innerHTML = `测试成功状态：<div>error：${JSON.stringify(error)}</div><div>dataInfo：${JSON.stringify(dataInfo)}</div>`;
         wrap.appendChild(div);
     },
 });
+// 测试失败状态
 jsonp({
-    url: 'http://www.suibianxiexie.top/phone/api/verify-code-register-random2/',
+    url: `https://api.douban.com/v2/book/search2`,
     data: {
-        isJsonp: true,
-        username: '1123486116@qq.com',
+        obj: {
+            a: 1,
+            b: '2',
+            c: [1, '2'],
+        },
+        array: [1, '2'],
+        hello: 'world',
+        num: 1,
     },
-    callbackName: 'hello',
     callback: function (error, dataInfo) {
         const div = document.createElement('div');
-        div.innerHTML = `<div>error：${JSON.stringify(error)}</div><div>dataInfo：${JSON.stringify(dataInfo)}</div>`;
+        div.innerHTML = `测试失败状态：<div>error：${JSON.stringify(error)}</div><div>dataInfo：${JSON.stringify(dataInfo)}</div>`;
         wrap.appendChild(div);
     },
 });
